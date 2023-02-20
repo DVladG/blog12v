c3f17d6f57a87cd2bd309f5201fd53229c44780c
DVladG <managedvg@gmail.com>
2023-02-20 16:30:02 +0200

    Initial commit

---
+++ blog12/src/test/java/com/experttrade/blog12/Blog12ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.experttrade.blog12;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Blog12ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
